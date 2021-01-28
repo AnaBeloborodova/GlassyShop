8f4a2535e5d016d61ae0c69fa4decbc124753b69
AnaBeloborodova <uroslovanasta@gmail.com>
2021-01-28 16:18:08 +0700

    :feat:add hover effects, map\

---
+++ dist/js/main.js
@@ -0,0 +1,52 @@
+// создание яндекс-карты
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938635, 30.323118],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш магазин',
+            balloonContent: 'Вход с торца'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '/dist/img/location-icon.svg',
+            // Размеры метки.
+            iconImageSize: [80, 140],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
+
+//создание слайдера
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
